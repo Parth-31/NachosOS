c428c9af12fae4c117b75329609031d056b57715
Parth-31 <parth.rupala@gmail.com>
2017-03-12 11:17:14 -0700

    final changes in PrioritySchedular.java for Proj1

--- threads/PriorityScheduler.java
+++ threads/PriorityScheduler.java
@@ -385,49 +385,13 @@ public class PriorityScheduler extends Scheduler {
 		HashMap<nachos.threads.PriorityScheduler.PriorityQueue,Long> waiting = new HashMap<nachos.threads.PriorityScheduler.PriorityQueue,Long>();
 	
 	}
-	/*
+	
 	public static void selfTest() {
-		ThreadQueue tq1 = ThreadedKernel.scheduler.newThreadQueue(true), tq2 = ThreadedKernel.scheduler.newThreadQueue(true), tq3 = ThreadedKernel.scheduler.newThreadQueue(true);
-		KThread kt_1 = new KThread(), kt_2 = new KThread(), kt_3 = new KThread(), kt_4 = new KThread();
 		
 		System.out.println("------------------------------------------------------------------");
-		
-		boolean status = Machine.interrupt().disable();
-		
-		tq1.waitForAccess(kt_1);
-		tq2.waitForAccess(kt_2);
-		tq3.waitForAccess(kt_3);
-		
-		tq1.acquire(kt_2);
-		tq2.acquire(kt_3);
-		tq3.acquire(kt_4);
-		
-		ThreadedKernel.scheduler.setPriority(kt_1, 6);
-		System.out.println("KT4: " + ThreadedKernel.scheduler.getPriority(kt_4));
+		System.out.println("PriorityScheduler.selfTest()");
 		
 		
-		Lib.assertTrue(ThreadedKernel.scheduler.getEffectivePriority(kt_4)==6);
-		
-		KThread kt_5 = new KThread();
-		
-		ThreadedKernel.scheduler.setPriority(kt_5, 7);
-		System.out.println("KT5: " + ThreadedKernel.scheduler.getPriority(kt_5));
-		
-		tq1.waitForAccess(kt_5);
-		
-		Lib.assertTrue(ThreadedKernel.scheduler.getEffectivePriority(kt_4)==7);
-		
-		tq1.nextThread();
-		
-		Lib.assertTrue(ThreadedKernel.scheduler.getEffectivePriority(kt_4)==1);
-		
-		System.out.println("KT1: " + ThreadedKernel.scheduler.getPriority(kt_1));
-		System.out.println("KT2: " + ThreadedKernel.scheduler.getPriority(kt_2));
-		System.out.println("KT3: " + ThreadedKernel.scheduler.getPriority(kt_3));
-		System.out.println("KT4: " + ThreadedKernel.scheduler.getPriority(kt_4));
-		
-		
-		Machine.interrupt().restore(status);
-	}*/
+	}
 }
 
