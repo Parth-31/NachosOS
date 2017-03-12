92edf04a676976c71b56ac7aebbc1865785fc371
Parth-31 <parth.rupala@gmail.com>
2017-03-12 11:16:16 -0700

    final changes in Condition2.java for Proj1

--- threads/Condition2.java
+++ threads/Condition2.java
@@ -2,6 +2,7 @@ package nachos.threads;
 
 import nachos.machine.*;
 
+
 /**
  * An implementation of condition variables that disables interrupt()s for
  * synchronization.
@@ -31,10 +32,12 @@ public class Condition2 {
 	 */
 	public void sleep() {
 		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
-
+		intState = Machine.interrupt().disable(); //for atomic operation
 		conditionLock.release();
-
+		waiting_queue.waitForAccess(KThread.currentThread());
+		KThread.sleep();
 		conditionLock.acquire();
+		Machine.interrupt().restore(intState);  //end of atomic operation
 	}
 
 	/**
@@ -43,15 +46,47 @@ public class Condition2 {
 	 */
 	public void wake() {
 		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+		
+		intState = Machine.interrupt().disable(); //for atomic operation
+		KThread t = waiting_queue.nextThread(); //get the next thread from the queue
+		if (t != null)
+			t.ready(); //Move this thread to ready queue
+		Machine.interrupt().restore(intState); // end of atomic execution
+		
 	}
 
 	/**
 	 * Wake up all threads sleeping on this condition variable. The current
 	 * thread must hold the associated lock.
 	 */
-	public void wakeAll() {
+	public void wakeAll() //similar to wake() but this loops through all the threads in the queue
+	{
 		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+		intState = Machine.interrupt().disable();
+		KThread thread = waiting_queue.nextThread();
+		while (thread != null) //iterate through all threads in the queue
+		{
+			thread.ready();
+			thread = waiting_queue.nextThread();
+		}
+		Machine.interrupt().restore(intState);
 	}
 
+	public static void selfTest() 
+	{
+		Lib.debug(dbgThread, "Enter Condition2.selfTest");
+
+		System.out.println("------------------------------------------------------------------");
+		
+		System.out.println("Condition2.selfTest()");
+		
+	}
+
+	
+
+	private static final char dbgThread = 't';
+
+	private boolean intState;
 	private Lock conditionLock;
+	private ThreadQueue waiting_queue= ThreadedKernel.scheduler.newThreadQueue(false); // no priority transfer => false
 }
